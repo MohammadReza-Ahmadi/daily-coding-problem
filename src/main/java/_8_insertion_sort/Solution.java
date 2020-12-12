fd3a739b28f3df390b3bd989ad673889d9335984
MohammadReza-Ahmadi <m.reza79ahmadi@gmail.com>
2020-12-12 22:57:55 +0330

    some changes

--- src/main/java/_8_insertion_sort/Solution.java
+++ src/main/java/_8_insertion_sort/Solution.java
@@ -3,19 +3,14 @@ package _8_insertion_sort;
 public class Solution {
 
     public static int[] insertionSort(int[] array) {
-        for (int i = 1; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             int newElement= array[i];
-            for (int j = i-1; j >=0 ; j--) {
+            for (int j = i; j >0  && newElement< array[j-1]; j--) {
+                array[i]=array[j-1];
             }
         }
         for (int i = 0; i < array.length; i++)
             System.out.print(array[i] + " ");
         return array;
-    }
-
-    public static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
     }
 }
