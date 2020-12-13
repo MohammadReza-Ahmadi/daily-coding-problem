639905ef53b2fefd8f7ea98d462f95ca819e4c83
mohammad-reza <mohammad-reza@mohammad-reza-ubuntu-20.04>
2020-12-13 15:53:25 +0330

    longestSpanwithSameSum commit

---
+++ src/main/java/_9_longest_span_with_same_sum/LongestSpanWithSameSum.java
@@ -0,0 +1,17 @@
+package _9_longest_span_with_same_sum;
+
+public class LongestSpanWithSameSum {
+
+    public static int longestSpanSum(boolean[] arr1, boolean[] arr2, int n) {
+        int maxLen = 0;
+        int preSum1 = 0;
+        int preSum2 = 0;
+        int[] diff = new int[2 * n + 1];
+//        memset(diff, -1, diff.length);
+//        for (int i = 0; i < n; i++) {
+//            preSum1 += arr1[i]==true? 1:0;
+//        }
+        return 0;
+
+    }
+}
