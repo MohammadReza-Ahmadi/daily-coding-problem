ad4c0f74d36c9dd1291d0920771a892c9fba1fa0
MohammadReza-Ahmadi <m.reza79ahmadi@gmail.com>
2020-12-18 10:07:21 +0330

    commit FindMissingNumber impl

---
+++ src/main/java/_15_find_missing_number_in_array/FindMissingNumber.java
@@ -0,0 +1,43 @@
+package _15_find_missing_number_in_array;
+
+public class FindMissingNumber {
+
+    public static int findMissNumber_method1(int[] array) {
+        int n = array.length;
+        int sum = (n + 1) * (n + 2) / 2;
+        for (int i=0; i<array.length; i++){
+            sum-=array[i];
+        }
+        System.out.println("method1', missNum="+sum);
+        return sum;
+    }
+
+    public static int findMissNumber_method2(int[] array) {
+        int x1 = array[0];
+        int x2 = 1;
+        for (int i = 1; i < array.length; i++) {
+//            System.out.print("arr[" + i + "]=" + array[i] + ", x1=" + x1);
+            x1 = x1 ^ array[i];
+//            System.out.print(", x1^arr[" + i + "]=" + x1);
+//            System.out.println();
+        }
+
+//        System.out.println("...... second for .......");
+        for (int i = 2; i <= array.length + 1; i++) {
+//            System.out.print("i=" + i + ", x2=" + x2);
+            x2 = x2 ^ i;
+//            System.out.print(", x2^i=" + x2);
+//            System.out.println();
+        }
+
+        System.out.println("method2, missNum="+(x1^x2));
+        return x1 ^ x2;
+    }
+
+    public static void main(String[] args) {
+        int a = 24;
+        int b = 20;
+        System.out.println(a ^ b);
+
+    }
+}
---
+++ src/test/java/_15_find_missing_number_in_array/FindMissingNumberTest.java
@@ -0,0 +1,24 @@
+package _15_find_missing_number_in_array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FindMissingNumberTest {
+
+    @Test
+    public void test_4_3(){
+        int[] arr = new int[]{1,2,4,5};
+        int expected = 3;
+        Assertions.assertEquals(expected,FindMissingNumber.findMissNumber_method1(arr));
+        Assertions.assertEquals(expected,FindMissingNumber.findMissNumber_method2(arr));
+    }
+
+    @Test
+    public void test_8_6(){
+        int[] arr = new int[]{1,2,3,4,5,7,8,9};
+        int expected = 6;
+        Assertions.assertEquals(expected,FindMissingNumber.findMissNumber_method1(arr));
+        Assertions.assertEquals(expected,FindMissingNumber.findMissNumber_method2(arr));
+    }
+
+}
