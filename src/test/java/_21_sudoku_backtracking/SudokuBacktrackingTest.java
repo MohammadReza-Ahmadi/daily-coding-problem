3e7d9399fd880b5a6b15f9fb43d6f769676ae3fa
mohammad-reza <mohammad-reza@mohammad-reza-ubuntu-20.04>
2020-12-19 11:21:12 +0330

    commit SudokuBacktracking

---
+++ src/test/java/_21_sudoku_backtracking/SudokuBacktrackingTest.java
@@ -0,0 +1,25 @@
+package _21_sudoku_backtracking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SudokuBacktrackingTest {
+
+
+    @Test
+    public void test_1(){
+        int grid[][] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+        SudokuBacktracking.getFilledSudoku(grid);
+    }
+
+}
